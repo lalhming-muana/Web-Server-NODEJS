4a557e71fb9a452c5c301c5ad03da1d2b35dbc06
“Muana” <lalhming.muana1982@gmail.com>
2023-02-12 13:00:36 +0530

    search form added to the UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,44 @@
 console.log('client side javascript code running');
+
+
+
+// fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
+
+//     response.json().then((data)=>{
+//         if(data.error){
+//             console.log('error cannot connect to the site!');
+//         }
+
+//         else{
+//             console.log('location: ',data.location);
+//             console.log('forecast: ',data.forecast);
+//         }
+
+//     })
+// })
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+
+    response.json().then((data)=>{
+        if(data.error){
+            console.log('error cannot connect to the site!');
+        }
+
+        else{
+            console.log('location: ',data.location);
+            console.log('forecast: ',data.forecast);
+        }
+
+    })
+})
+
+    
+})
